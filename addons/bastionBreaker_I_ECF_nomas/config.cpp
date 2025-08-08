a31eaa7487044811f3a856a14f98119a2c98561a
Nomas <nomasx2000@gmail.com>
2025-08-08 04:19:14 +0300

    Fixed Bastion Breaker ECF by removing plane that had wrongt logo

--- addons/bastionBreaker_I_ECF_nomas/config.cpp
+++ addons/bastionBreaker_I_ECF_nomas/config.cpp
@@ -3,7 +3,7 @@ class cfgPatches
   class AET_F_bastionBreaker_I_ECF_nomas
   {
 	skipWhenMissingDependencies = 1;
-    units[]={"AET_F_bastionBreaker_I_ECF_Rifleman","AET_F_bastionBreaker_I_ECF_Medic","AET_F_bastionBreaker_I_ECF_AT_Specialist","AET_F_bastionBreaker_I_ECF_AA_Specialist","AET_F_bastionBreaker_I_ECF_Marksman","AET_F_bastionBreaker_I_ECF_Machine_Gunner","AET_F_bastionBreaker_I_ECF_UAV_Operator","AET_F_bastionBreaker_I_ECF_Squad_Leader","AET_F_bastionBreaker_I_ECF_Crewman","AET_F_bastionBreaker_I_ECF_Officer","AET_F_bastionBreaker_I_ECF_Skimmer_Drone_Man","AET_F_bastionBreaker_I_ECF_SF_Sniper","AET_F_bastionBreaker_I_ECF_SF_Spotter","AET_F_bastionBreaker_I_ECF_SF_Team_Leader","AET_F_bastionBreaker_I_ECF_SF_Rifleman_SG","AET_F_bastionBreaker_I_ECF_SF_Rifleman","AET_F_bastionBreaker_I_ECF_SF_Marksman","AET_F_bastionBreaker_I_ECF_SF_AT_Specialist","AET_F_bastionBreaker_I_ECF_SF_Medic","AET_F_bastionBreaker_I_ECF_SF_AA_Specialist","AET_F_bastionBreaker_I_ECF_Helicopter_Pilot","AET_F_bastionBreaker_I_ECF_Pilot","AET_F_bastionBreaker_I_ECF_TC_15_Bearcat_SHORAD","AET_F_bastionBreaker_I_ECF_TC_15_Bearcat_APC","AET_F_bastionBreaker_I_ECF_TC_15_Bearcat_FSV","AET_F_bastionBreaker_I_ECF_TC_15_Bearcat_IFV","AET_F_bastionBreaker_I_ECF_AH_44_A_Dragonfly","AET_F_bastionBreaker_I_ECF_AH_44_S_Dragonfly","AET_F_bastionBreaker_I_ECF_General_Utility_Shuttle_A","AET_F_bastionBreaker_I_ECF_General_Utility_Shuttle_M","AET_F_bastionBreaker_I_ECF_TF_3_Fighter"};
+    units[]={"AET_F_bastionBreaker_I_ECF_Rifleman","AET_F_bastionBreaker_I_ECF_Medic","AET_F_bastionBreaker_I_ECF_AT_Specialist","AET_F_bastionBreaker_I_ECF_AA_Specialist","AET_F_bastionBreaker_I_ECF_Marksman","AET_F_bastionBreaker_I_ECF_Machine_Gunner","AET_F_bastionBreaker_I_ECF_UAV_Operator","AET_F_bastionBreaker_I_ECF_Squad_Leader","AET_F_bastionBreaker_I_ECF_Crewman","AET_F_bastionBreaker_I_ECF_Officer","AET_F_bastionBreaker_I_ECF_Skimmer_Drone_Man","AET_F_bastionBreaker_I_ECF_SF_Sniper","AET_F_bastionBreaker_I_ECF_SF_Spotter","AET_F_bastionBreaker_I_ECF_SF_Team_Leader","AET_F_bastionBreaker_I_ECF_SF_Rifleman_SG","AET_F_bastionBreaker_I_ECF_SF_Rifleman","AET_F_bastionBreaker_I_ECF_SF_Marksman","AET_F_bastionBreaker_I_ECF_SF_AT_Specialist","AET_F_bastionBreaker_I_ECF_SF_Medic","AET_F_bastionBreaker_I_ECF_SF_AA_Specialist","AET_F_bastionBreaker_I_ECF_Helicopter_Pilot","AET_F_bastionBreaker_I_ECF_Pilot","AET_F_bastionBreaker_I_ECF_TC_15_Bearcat_SHORAD","AET_F_bastionBreaker_I_ECF_TC_15_Bearcat_APC","AET_F_bastionBreaker_I_ECF_TC_15_Bearcat_FSV","AET_F_bastionBreaker_I_ECF_TC_15_Bearcat_IFV","AET_F_bastionBreaker_I_ECF_AH_44_A_Dragonfly","AET_F_bastionBreaker_I_ECF_AH_44_S_Dragonfly","AET_F_bastionBreaker_I_ECF_General_Utility_Shuttle_A","AET_F_bastionBreaker_I_ECF_General_Utility_Shuttle_M"};
     weapons[]={"AET_F_bastionBreaker_I_ECF_SC_Rifle_AR12_acc_flashlightoptic_SRS","AET_F_bastionBreaker_I_ECF_SC_Pistol_SSP9","AET_F_bastionBreaker_I_ECF_SC_Rifle_AR12C_acc_flashlightoptic_SRS","AET_F_bastionBreaker_I_ECF_SC_MPML_SC_MPML_Scope","AET_F_bastionBreaker_I_ECF_launch_B_Titan_olive_F","AET_F_bastionBreaker_I_ECF_SC_Rifle_AR12_acc_flashlightSC_TSO55","AET_F_bastionBreaker_I_ECF_SC_Rifle_PKMP_optic_SRS","AET_F_bastionBreaker_I_ECF_SC_Rifle_Ghoul_acc_flashlightoptic_Yorris","AET_F_bastionBreaker_I_ECF_SC_SkimmerDroneGun","AET_F_bastionBreaker_I_ECF_SC_Rifle_SSR55_SC_TSO55bipod_01_F_blk","AET_F_bastionBreaker_I_ECF_SC_Rifle_SSR55C_SC_TSO55bipod_01_F_blk","AET_F_bastionBreaker_I_ECF_arifle_MSBS65_GL_black_F_muzzle_snds_65_TI_blk_Facc_pointer_IRoptic_ico_01_black_f","AET_F_bastionBreaker_I_ECF_arifle_MSBS65_black_F_muzzle_snds_65_TI_blk_Facc_pointer_IRoptic_ico_01_black_f","AET_F_bastionBreaker_I_ECF_arifle_MSBS65_UBS_black_F_muzzle_snds_65_TI_blk_Facc_pointer_IRoptic_ico_01_black_f","AET_F_bastionBreaker_I_ECF_arifle_MSBS65_Mark_black_F_muzzle_snds_65_TI_blk_Facc_pointer_IRoptic_ico_01_black_fbipod_01_F_blk"};
     requiredVersion=0.1;
     requiredAddons[]={"ace_medical_treatment","A3_Weapons_F","sc_weapons","screborn_uniforms","A3_Characters_F","A3_Weapons_F_Items","A3_Weapons_F_Enoch_Launchers_Titan","SC_RebornUnits_Earth_Colonies_Federation","A3_Drones_F_Weapons_F_Gamma_Items","SC_Scion_Empire_Metro_Police_Scion_Empire_Metro_Police","sc_newequipment","SC_newequipment2","A3_Weapons_F_Enoch_Rifles_MSBS"};
@@ -337,7 +337,6 @@ class cfgVehicles
   class TKE_Ext_Dragonfly_S;
   class TKE_Ext_GUSA;
   class TKE_Ext_GUSM;
-  class SCR_TF3Fighter_SE;
 
   class AET_F_bastionBreaker_I_ECF_Rifleman: I_soldier_F
   {
@@ -789,16 +788,6 @@ class cfgVehicles
     side=2;
     displayName="General Utility Shuttle M";
     hiddenSelectionsTextures[]={"tke_ext_shuttle\data\shuttle_green_co.paa"};
-    crew="AET_F_bastionBreaker_I_ECF_Pilot";
-    typicalCargo[]={"AET_F_bastionBreaker_I_ECF_Pilot"};
-  };
-
-  class AET_F_bastionBreaker_I_ECF_TF_3_Fighter: SCR_TF3Fighter_SE
-  {
-    faction="AET_F_bastionBreaker_I_ECF_nomas";
-    side=2;
-    displayName="TF 3 Fighter";
-    hiddenSelectionsTextures[]={"sc_ext_fixed\data\tf3\textures\body_black_se_co.paa","sc_fixed\data\fixed_bomber_01\details\details_co.paa","sc_ext_fixed\data\tf3\textures\wings_black_co.paa"};
     crew="AET_F_bastionBreaker_I_ECF_Pilot";
     typicalCargo[]={"AET_F_bastionBreaker_I_ECF_Pilot"};
   };
--- addons/main/script_version.hpp
+++ addons/main/script_version.hpp
@@ -1,7 +1,7 @@
 #define MAJOR 1
 #define MINOR 2
-#define PATCH 1
-#define BUILD 843
+#define PATCH 2
+#define BUILD 845
 
 // #define VERSION MACROS
 #define VERSION MAJOR.MINOR
